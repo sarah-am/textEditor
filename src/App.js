c26d3aabc8ce9b110021d6d75c332ba14e15ca8f
DarthHamza <hamzamakia@gmail.com>
2018-10-31 17:24:17 +0300

    Aziz's super solution

--- src/App.js
+++ src/App.js
@@ -8,21 +8,59 @@ const styles = {
 };
 
 class App extends Component {
-  render() {
-    let stylings = ["bold", "italic", "underline"];
-    let colors = ["yellow", "blue", "red", "black", "purple"];
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: "black",
+      bold: false,
+      italic: false,
+      underline: false
+    };
 
-    let stylingBoxes = stylings.map(style => {
+    this.stylings = ["bold", "italic", "underline"];
+    this.colors = ["yellow", "blue", "red", "black", "purple"];
+
+    this.setStyle = this.setStyle.bind(this);
+  }
+
+  setStyle(style) {
+    this.setState(prevState => ({ [style]: !prevState[style] }));
+  }
+
+  setColor(color) {
+    this.setState({ color });
+  }
+
+  getStyle() {
+    let style = {
+      color: this.state.color
+    };
+
+    if (this.state.bold) style = { ...style, ...styles.bold };
+    if (this.state.italic) style = { ...style, ...styles.italic };
+    if (this.state.underline) style = { ...style, ...styles.underline };
+
+    return style;
+  }
+
+  render() {
+    let stylingBoxes = this.stylings.map(style => {
       return (
-        <button style={styles[style]} key={style}>
+        <button
+          className={`btn ${this.state[style] && "btn-primary"}`}
+          onClick={() => this.setStyle(style)}
+          style={styles[style]}
+          key={style}
+        >
           {style}
         </button>
       );
     });
 
-    let colorBoxes = colors.map(color => {
+    let colorBoxes = this.colors.map(color => {
       return (
         <button
+          onClick={() => this.setColor(color)}
           style={{ backgroundColor: color, height: 30, width: 30 }}
           key={color}
         />
@@ -35,7 +73,7 @@ class App extends Component {
         {stylingBoxes}
         <br />
         <br />
-        <textarea />
+        <textarea style={this.getStyle()} />
         <br />
         {colorBoxes}
       </div>
